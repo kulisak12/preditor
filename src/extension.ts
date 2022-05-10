15db2ef74cc9e155d4ef6c55b46ed92bae04baf1
kulisak12 <kulisak12lp@gmail.com>
2022-05-10 09:02:30 +0200

    Suggest last 3 words

--- src/extension.ts
+++ src/extension.ts
@@ -2,24 +2,43 @@
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
+const TRIGGER_CHARS = [
+	" ",
+	".",
+	",",
+	":",
+	"!",
+	"?",
+	"(",
+	")",
+	"'",
+	'"',
+	"/",
+];
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "prediktor" is now active!');
+	// ? provideCompletionItems can be async
+	const provider = vscode.languages.registerCompletionItemProvider(
+		{ pattern: "**" },
+		{
+			provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('prediktor.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from prediktor!');
-	});
+				const linePrefix = document.lineAt(position).text.substring(0, position.character);
+				const words = linePrefix.split(' ');
+				const arbitraryIndex = Math.max(0, words.length - 4);
+				const completionWords = words.slice(arbitraryIndex);
+				const completionsItems = completionWords.map(word => new vscode.CompletionItem(word), vscode.CompletionItemKind.Text);
 
-	context.subscriptions.push(disposable);
+				return completionsItems;
+			}
+		},
+		...TRIGGER_CHARS
+		);
+
+	context.subscriptions.push(provider);
 }
 
 // this method is called when your extension is deactivated
