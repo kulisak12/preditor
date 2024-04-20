bb338631e5d3519d250d035cd3357f59d7e59eda
kulisak12 <kulisak12lp@gmail.com>
2024-04-20 20:00:50 +0200

    Add models

---
+++ src/models.ts
@@ -0,0 +1,31 @@
+import * as vscode from 'vscode';
+
+export class SuggestionRequest {
+    beforeCursor: string;
+    afterCursor: string;
+    predictionConfig: vscode.WorkspaceConfiguration;
+    infillingConfig: vscode.WorkspaceConfiguration;
+
+    constructor(before: string, after: string, predictionConfig: vscode.WorkspaceConfiguration, infillingConfig: vscode.WorkspaceConfiguration) {
+        this.beforeCursor = before;
+        this.afterCursor = after;
+        this.predictionConfig = predictionConfig;
+        this.infillingConfig = infillingConfig;
+    }
+}
+
+export class SubstitutionRequest {
+    text: string;
+    start: number;
+    length: number;
+    replacement: string;
+    config: vscode.WorkspaceConfiguration;
+
+    constructor(text: string, start: number, length: number, replacement: string, config: vscode.WorkspaceConfiguration) {
+        this.text = text;
+        this.start = start;
+        this.length = length;
+        this.replacement = replacement;
+        this.config = config;
+    }
+}
